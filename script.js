3d2e696b5757d26531eca8baef98eaa5589f9032
Dr Phil <philip.prewitt@gmail.com>
2020-07-28 21:24:38 -0600

    got the API working

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var endYear = $("#endYear").val();
     var APIKey = "wdC2mcsgCZbxKVDBM8v7mr5cwP2Bw9Xx";
 
     // Here we are building the URL we need to query the database
-    var queryURL = "http://api.nytimes.com/svc/archive/v1/2016/11.json?api-key=" + APIKey;
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+search+"&api-key=" + APIKey;
     console.log(queryURL);
 
     //Create the AJAX call 
