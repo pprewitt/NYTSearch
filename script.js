56a43498e4752644b313c7e1f1a5affd098cdcb7
Dr Phil <philip.prewitt@gmail.com>
2020-07-28 21:53:27 -0600

    got the API to accept the search input

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
     $(document).ready(function(){
-
-var search = $("#search").val().trim();
-var numRecords = $("#numRecords").val();
-var startYear = $("startYear").val();
-var endYear = $("#endYear").val();  
+    $("#submit").click(function(event){
+        event.preventDefault();
+    var search = $("#search").val().trim();
+    console.log(search)
+    var numRecords = $("#numRecords").val();
+    var startYear = $("startYear").val();
+    var endYear = $("#endYear").val();  
     
     // This is our API key. Add your own API key between the ""
     var APIKey = "wdC2mcsgCZbxKVDBM8v7mr5cwP2Bw9Xx";
@@ -19,5 +21,9 @@ var endYear = $("#endYear").val();
     }).done(function(response) {
         console.log(response);
       });
+    })
 
+    
     });
+
+    // "&facet_fields=source&facet=true&begin_date="+startYear+"0101&end_date="+endYear+"1231
